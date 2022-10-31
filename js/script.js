cd8005c369168f363b4655d1aefe9bd37c44a774
j-faust <85812553+j-faust@users.noreply.github.com>
2022-10-31 11:42:14 -0400

    Added JS and JQUERY code to save text entered to calendar to local storage on click

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 // Display current date and time on Jumbotron on the mainpage
 var date = new Date();
 document.getElementById("currentDay").innerHTML = date;
+
+
+// Function that will be used for save button click event to save calendar text to localStorage
+function saveToCalendar() {
+    let eventText = [];
+    for(let i = 0; i < 9; i++) {
+        let id = '#calendarTextarea' + i.toString();
+        let eventEl = $(id)[0];
+        console.log(eventEl.value);
+        eventText.push(eventEl.value);
+    }
+    let jsonString = JSON.stringify(eventText);
+    localStorage.setItem("eventText", jsonString);
+    
+}
+
+
+// Function to load saved text onto calendar from localStorage
+function reloadToCalendar() {
+    let eventText = [];
+    let text = localStorage.getItem("eventText");
+
+    // if statement to check if there is text in localStorage to before loading to the calendar 
+    if(text !== undefined && text !== null && text !== ""){
+    eventText = JSON.parse(text);
+    for(let i = 0; i <= 9; i++) {
+        eventText.push($('#calendarTextarea' + i.toString()).value);
+        }
+    }
+}
+
+window.onload = reloadToCalendar();
+
+// event listener for cick to save text entered to calendar
+$('Button.saveBtn').click(saveToCalendar);
+
+
