bfd98eee493204617eea833f3a9a4a38061d4c3c
j-faust <85812553+j-faust@users.noreply.github.com>
2022-11-01 08:13:47 -0400

    Added final comments to code

--- js/script.js
+++ js/script.js
@@ -7,12 +7,11 @@ let currentHour = date.getHours(); // setting variable of currentHour using Java
 
 // Function that will be used for save button click event to save calendar text to localStorage
 function saveToCalendar() {
-    let eventText = [];
+    let eventText = []; // empty array to store the calendar information in for localStorage
     for(let i = 0; i < 9; i++) {
         let id = '#calendarTextarea' + i.toString(); // setting id variable for all of the text entered into the calendar for each hour
-        let eventEl = $(id)[0];
-        console.log(eventEl.value); // 
-        eventText.push(eventEl.value); // 
+        let eventEl = $(id)[0]; // using JQUERY to set id for evenEl and set to start at the 0 index of the array and work through each one after that
+        eventText.push(eventEl.value); // pushes the values s
     }
     let jsonString = JSON.stringify(eventText); // setting jsonString variable to the stringified evenText from the textarea on the calendar
     localStorage.setItem("eventText", jsonString); // saving text in textarea on calendar to localStorage 
@@ -23,16 +22,16 @@ let startHour = 9 //setting variable of startHour equal to value of 9 to represe
 
 // Function to load saved text onto calendar from localStorage
 function reloadToCalendar() {
-    let eventText = [];
-    let text = localStorage.getItem("eventText");
+    let eventText = []; // array storing saved calendar data
+    let text = localStorage.getItem("eventText"); // setting variable that retrieves data from localStorage
 
-    // if statement to check if there is text in localStorage before loading to the calendar 
+    // if statement to check if there is text in localStorage before loading to the calendar and use JSON to parse the information
     if(text !== undefined && text !== null && text !== ""){
-    eventText = JSON.parse(text); // using JSON parse to bring data from localStorage to correctly show to the user
+    eventText = JSON.parse(text); 
 
     //for loop set to loop through each row of the calendar and color code it correctly as well as 
     for(let i = 0; i <= 9; i++) {
-        let hour = startHour + i; 
+        let hour = startHour + i; // hour variable that will work through each time-block
         let id = '#calendarTextarea' + i.toString() // added id variable to call in the eventbox.
         let eventBox = $(id); // using jquery to call id and set variable 'evenBox' equal to it.
         eventBox[0].value = eventText[i]; 
